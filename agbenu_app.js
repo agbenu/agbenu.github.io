26a0ef48a90596b6b6935c90ebb40c89e6a3e067
Eli Agbenu <eli.anthony.agbenu@gmail.com>
2016-09-27 00:22:40 +0000

    updated angular files

--- agbenu_app.js
+++ agbenu_app.js
@@ -14,10 +14,16 @@ angular.module('agbenuApp', ['ngRoute'])
                 controller:"PythonPageController"
             })
 
+            .when("/gist/code/python",{
+                templateUrl: "./templates/gist_code.html",
+                controller:"GistCodeController"
+            })            
+
             .when("/android",{
                 templateUrl: "./templates/android.html",
                 controller:"AndroidPageController"
             }) 
+
 
             .otherwise('/',
             {
@@ -40,16 +46,71 @@ angular.module('agbenuApp', ['ngRoute'])
 
     .controller('PythonPageController', 
 
-            ['$scope', function ($scope) {
+            ['$scope', 'GistService', function ($scope,GistService) {
 
             $scope.welcome_text = 'Python is really good for text processing and data handling';
 
+
             var python_tutorials = [
-               { link: 'django/nginx_django.html', link_title:'Django config sample with Nginx' } ,
-               { link: 'django/multiple_apps_nginx_port.html', link_title:'Multiple apps from a single Nginx port' } ,
-               { link: 'django/custom_user_model.html', link_title:'Custom user model' } ,                              
+               { 
+                link: '/gist/code/python', 
+                link_title:'Django config sample with Nginx', 
+                gist_link: 'https://gist.github.com/lefootballroi/25c66b5cd6a1cea7a2f4fa094e8c4b5b',
+                code_language: 1
+           } ,
+               { 
+                link: '/gist/code/python', 
+                link_title:'Multiple apps from a single Nginx port', 
+                gist_link: 'https://gist.github.com/lefootballroi/25c66b5cd6a1cea7a2f4fa094e8c4b5b',
+                code_language: 1                                 
+            } ,
+               { 
+                link: '/gist/code/python', 
+                link_title:'Custom user model',
+                gist_link: 'https://gist.github.com/lefootballroi/25c66b5cd6a1cea7a2f4fa094e8c4b5b',
+                code_language: 1                                  
+           } ,                              
             ];
 
             $scope.python_tutorials = python_tutorials;
+
+            $scope.setPythonTutorial= function(this_gist_link) {
+                    GistService.setTutorial(this_gist_link);                                           
+                }
+
+
+        }])    
+
+
+    .controller('GistCodeController', 
+            ['$scope', 'GistService' ,function ($scope,GistService) 
+            {
+
+                var this_gist_link =  GistService.getTutorial(); 
+
+                $scope.this_gist_link = this_gist_link;
                 
-        }]);    
+                console.log(this_gist_link);                                                                   
+             
+        }])
+
+
+    .service('GistService', function () 
+        {
+            var gist_link="xxx";
+            return {
+
+                    setTutorial: function (this_gist_link){ 
+                        gist_link = this_gist_link;
+                        //console.log(gist_link);                                                   
+                    },
+
+                    getTutorial: function (){ 
+                        return gist_link;
+                    },
+
+                };
+          
+        });    
+
+    
